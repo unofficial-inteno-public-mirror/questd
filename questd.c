a567e509f9e10b6d399e367acec2f2e79e38b078
Sukru Senli <sukru.senli@inteno.se>
2016-09-05 12:13:58 +0200

    remove mutex lock from reload function

--- questd.c
+++ questd.c
@@ -2144,13 +2144,11 @@ quest_reload(struct ubus_context *ctx, struct ubus_object *obj,
 		  struct ubus_request_data *req, const char *method,
 		  struct blob_attr *msg)
 {
-	pthread_mutex_lock(&lock);
 	dump_hostname(&router);
 	load_networks();
 #if IOPSYS_BROADCOM
 	load_wireless();
 #endif
-	pthread_mutex_unlock(&lock);
 	return 0;
 }
 
