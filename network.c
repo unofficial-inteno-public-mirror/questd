60f8a4f011193318940957f953fc8325555d6019
Sukru Senli <sukru.senli@inteno.se>
2016-11-18 18:03:31 +0100

    mtk: show speed correctly

--- network.c
+++ network.c
@@ -515,7 +515,7 @@ router_dump_ports(struct blob_buf *b, char *interface)
 	ports[7] = "WLAN";
 
 	Port *port;
-	
+
 	for (i = 0; i < MAX_NETWORK; i++) {
 		if (network[i].exists && !strcmp(network[i].name, interface)) {
 			populate_ports(&network[i]);
@@ -524,7 +524,7 @@ router_dump_ports(struct blob_buf *b, char *interface)
 			break;
 		}
 	}
-	
+
 	if (!found)
 		return;
 
